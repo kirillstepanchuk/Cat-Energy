35aa6771e74f45d9e77209fa85f5e34515034e49
kirillstepanchuk <kirill.stepanchuk.96@mail.ru>
2020-10-06 21:14:07 +0300

    Доверстал все верисии макета

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -7,7 +7,7 @@ navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
-	} else {
+  } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
